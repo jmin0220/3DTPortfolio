959a9491871b16fe0bfa54bd7447d1541c1996b6
LeeJungmin <apple0220@naver.com>
2022-10-17 18:29:32 +0900

    이정민 : 프로젝트 생성 및 엔진 업데이트

---
+++ DirectX/3DTPortfolio/GameEngineContents/ContentsCore.cpp
@@ -0,0 +1,26 @@
+#include "PreCompile.h"
+#include "ContentsCore.h"
+
+#pragma comment(lib, "GameEngineBase.lib")
+
+ContentsCore::ContentsCore() 
+	: GameEngineCore()
+{
+}
+
+ContentsCore::~ContentsCore() 
+{
+}
+
+void ContentsCore::Start()
+{
+
+}
+
+void ContentsCore::Update(float _DeltaTime)
+{
+}
+
+void ContentsCore::End()
+{
+}
