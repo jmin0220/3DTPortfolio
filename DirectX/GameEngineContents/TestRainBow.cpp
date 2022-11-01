2292d1fba259f228f9a28c6a80daabebef0669e2
희상 황 <gmltkd0423@naver.com>
2022-11-01 18:43:11 +0900

    황희상 : 메쉬 로드 업데이트

---
+++ DirectX/GameEngineContents/TestRainBow.cpp
@@ -0,0 +1,28 @@
+#include "PreCompile.h"
+#include "TestRainBow.h"
+#include <GameEngineCore/GameEngineFBXRenderer.h>
+
+
+TestRainBow::TestRainBow() 
+{
+}
+
+TestRainBow::~TestRainBow() 
+{
+}
+
+void TestRainBow::Start()
+{
+	{
+		GameEngineFBXRenderer* Renderer = CreateComponent<GameEngineFBXRenderer>();
+		Renderer->SetFBXMesh("Rainbow.FBX", "Texture");
+		Renderer->GetTransform().SetWorldScale({ 50.0f,50.0f, 50.0f});
+		// Renderer->SetFBXMesh("Monster3.FBX", "Texture");
+		// std::vector<std::vector<GameEngineRenderUnit>>& Meshs = Renderer->GetAllRenderUnit();
+	}
+}
+
+void TestRainBow::Update(float _DeltaTime)
+{
+}
+
---
+++ DirectX/GameEngineContents/TestRainBow.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <GameEngineCore/CoreMinimal.h>
+
+
+// Ό³Έν :
+class TestRainBow	:	public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	TestRainBow();
+	~TestRainBow();
+
+	// delete Function
+	TestRainBow(const TestRainBow& _Other) = delete;
+	TestRainBow(TestRainBow&& _Other) noexcept = delete;
+	TestRainBow& operator=(const TestRainBow& _Other) = delete;
+	TestRainBow& operator=(TestRainBow&& _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+private:
+
+};
+
