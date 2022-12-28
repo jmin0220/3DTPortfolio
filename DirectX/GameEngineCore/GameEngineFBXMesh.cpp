1fa342b507f566e89cc932e26f56dc85d128002b
류진아 <12rotus31@gmail.com>
2022-12-28 15:39:15 +0900

    류진아 : 노말맵 수정 작업 및 피직스머테리얼 일부 적용

--- DirectX/GameEngineCore/GameEngineFBXMesh.cpp
+++ DirectX/GameEngineCore/GameEngineFBXMesh.cpp
@@ -405,7 +405,9 @@ void GameEngineFBXMesh::LoadTangent(fbxsdk::FbxMesh* _Mesh, fbxsdk::FbxAMatrix _
 
 	if (0 == iCount)
 	{
-		return;
+		// ***탄젠트 정보가 없는경우 탄젠트를 생성***(선생님 코드와 다름)
+		_Mesh->GenerateTangentsData(0);
+		//return;
 	}
 	FbxGeometryElementTangent* pElement = _Mesh->GetElementTangent();
 	int iDataIndex = VtxId;
